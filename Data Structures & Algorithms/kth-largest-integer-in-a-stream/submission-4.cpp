93d204498553e9e1a6c7bcc7481fa71279f396d6
ankithreddy571 <70102630+ankithreddy571@users.noreply.github.com>
2026-08-01 16:49:17 -0700

    Add: kth-largest-integer-in-a-stream - submission-4

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-4.cpp
@@ -0,0 +1,23 @@
+class KthLargest {
+public:
+    int k;
+    priority_queue<int,vector<int>,greater<int>> minHeap;
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for(int num : nums){
+            add(num);
+        }      
+    }
+    
+    int add(int val) {
+        if(minHeap.size() < k){
+            minHeap.push(val);
+        }else if(val > minHeap.top()){
+            minHeap.push(val);
+            while(minHeap.size() > k){
+                minHeap.pop();
+            }
+        }
+        return minHeap.top();
+    }
+};
